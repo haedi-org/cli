3993e9fcc99bd8ef1d15db2dc6c506a6fdf9a115
Louis Machin <ldhmachin@gmail.com>
2021-08-05 09:29:17 +0100

    Removed composite version from elements

--- lib/segments/unh.rb
+++ lib/segments/unh.rb
@@ -1,20 +1,25 @@
 class UNH < Line
+    attr_reader :message_version
     attr_reader :message_reference
     attr_reader :message_type
-    attr_reader :message_version
+    attr_reader :message_version_number
+    attr_reader :message_release_number
     attr_reader :controlling_agency
     attr_reader :association_code
 
     def initialize(data, line_no, version, chars)
         super(data, line_no, version, chars)
+        @message_version = format_version
         # Push to elements
         push_elements([
-            # (0052) Message version number
-            @message_version = format_version,
             # (0062) Message reference number
             @message_reference = define([1, 0], "0062"),
             # (0065) Message type
             @message_type = define([2, 0], "0065", true, @message_version.ref),
+            # (0052) Message version number
+            @message_version_number = define([2, 1], "0052"),
+            # (0054) Message release number
+            @message_release_number = define([2, 2], "0054"),
             # (0051) Controlling agency, coded
             @controlling_agency = define([2, 3], "0051", true, @message_version.ref),
             # (0057) Association assigned code
