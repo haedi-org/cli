fc004ea5dad5ffabe80543a0d404cf834beecd2d
Louis Machin <ldhmachin@gmail.com>
2021-12-19 23:40:56 +0000

    Added ISO 6346 checksum validation

--- lib/datatypes.rb
+++ lib/datatypes.rb
@@ -76,6 +76,41 @@ class String
                 false
         end
     end
+
+    ISO_6346_TRANSLITERATION_MAP = {
+        'A' => 10, 'B' => 12, 'C' => 13, 'D' => 14, 'E' => 15, 'F' => 16,
+        'G' => 17, 'H' => 18, 'I' => 19, 'J' => 20, 'K' => 21, 'L' => 23,
+        'M' => 24, 'N' => 25, 'O' => 26, 'P' => 27, 'Q' => 28, 'R' => 29,
+        'S' => 30, 'T' => 31, 'U' => 32, 'V' => 34, 'W' => 35, 'X' => 36,
+        'Y' => 37, 'Z' => 38,
+    }
+
+    ISO_6346_CATEGORY_IDS = ['U', 'J', 'Z']
+
+    def is_iso_6346?
+        # Check for correct length
+        return false unless self.length == 11
+        # Check 4th character is a valid category identifier
+        return false unless ISO_6346_CATEGORY_IDS.include?(self.chars[3])
+        # Step 1: Transliterate
+        values = []
+        for char in self.upcase.chars[0, 10] do
+            if ('A'..'Z').to_a.include?(char)
+                values << ISO_6346_TRANSLITERATION_MAP[char]
+            else
+                values << char.to_i
+            end
+        end
+        # Step 2: Multiply by n^index
+        values = values.map.with_index do |value, index|
+            value * (2 ** index)
+        end
+        # Step 3: Compute check digit
+        expected_check_digit = values.sum - (11 * (values.sum / 11).floor)
+        # Step 4: Compare remainder to check digit (the 11th character)
+        given_check_digit = self.chars[10]
+        return given_check_digit == expected_check_digit.to_s
+    end
     
     VIN_ILLEGAL_CHARACTERS = ['I', 'O', 'Q']
 
